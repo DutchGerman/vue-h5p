a90e1b93c9ede9e33aefe18694d96832e5c434ff
StefanV1sser <stefan.visser@apm-ecampus.de>
2020-08-11 15:52:51 +0200

    refactor: select correct element for tests

--- tests/unit/h5p.spec.js
+++ tests/unit/h5p.spec.js
@@ -39,7 +39,7 @@ describe('Component', () => {
       src: '/hello-world'
     }))
     await flushPromises()
-    expect(wrapper.element.constructor.name).toBe('HTMLIFrameElement')
+    expect(wrapper.find('iframe').element.constructor.name).toBe('HTMLIFrameElement')
   })
 
   it('renders default slot content while loading', async () => {
@@ -105,7 +105,7 @@ describe('Component', () => {
         src: '/hello-world'
       }))
       await flushPromises()
-      expect(wrapper.element.srcdoc).toMatchSnapshot()
+      expect(wrapper.find('iframe').element.srcdoc).toMatchSnapshot()
     })
 
     it('has sorted dependencies', async () => {
@@ -113,7 +113,7 @@ describe('Component', () => {
         src: '/course-presentation'
       }))
       await flushPromises()
-      expect(wrapper.element.srcdoc).toMatchSnapshot()
+      expect(wrapper.find('iframe').element.srcdoc).toMatchSnapshot()
     })
 
     it('without version in library paths', async () => {
@@ -121,7 +121,7 @@ describe('Component', () => {
         src: '/hello-world-no-version'
       }))
       await flushPromises()
-      expect(wrapper.element.srcdoc).toMatchSnapshot()
+      expect(wrapper.find('iframe').element.srcdoc).toMatchSnapshot()
     })
 
     it('emits h5p events', () => {
@@ -144,7 +144,7 @@ describe('Component', () => {
         resize: 'resize code'
       }))
       await flushPromises()
-      expect(wrapper.element.srcdoc).toMatchSnapshot()
+      expect(wrapper.find('iframe').element.srcdoc).toMatchSnapshot()
     })
   })
 })
